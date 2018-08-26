add4c4408ecb1d95f78b53344725943ad36a6dd9
vagrant <vagrant@homestead>
2018-08-26 10:03:47 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由<a href="http://www.seo0769.com" target="_blank">Seo0769</a> 设计编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:seo@seo0769.com">联系我们</a></p>
+    </div>
+</footer>
